20aae6eaf378563ef02d6b2d47f8414e2d08def3
MikroDataBase <basedatos076@gmail.com>
2025-05-18 02:11:59 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $host = "https://mikrotik-qwkw.vercel.app/"; // Cambia esto por tu host de MySQL
 $username = "root"; // Cambia esto por tu usuario de MySQL
 $password = ""; // Cambia esto a "" si tu usuario root no tiene contraseña
-$database = "mikrotik_users"; // Cambia esto por el nombre de tu base de datos
+$database = "mikrotik-qwkw"; // Cambia esto por el nombre de tu base de datos
 
 // Crear conexión
 $conn = new mysqli($host, $username, $password, $database);
